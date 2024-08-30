94877f12bf9e77f2aaa3c5ccdd7c8943f9ba9f62
Droid-Jr <droidjr10@gmail.com>
2024-08-29 22:03:21 -0500

    <mario>: tipado en base de datos conexcion y agrege interfas de conexcion

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,17 @@
 import { Sequelize } from "sequelize";
+import { DBConfig } from "../interface/db.interface";
+require("dotenv").config();
+
+const { DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT } =
+  process.env as unknown as DBConfig;
 
 const B2PE_DB = new Sequelize(
-  "B2PE_DB", // database name
-  "root", // username
-  "root", // password
+  DB_DATABASE, // database name
+  DB_USERNAME, // username
+  DB_PASSWORD, // password
   {
-    host: "localhost",
-    port: 3306, // port should be a number, not a string
+    host: DB_HOST,
+    port: DB_PORT, // port should be a number, not a string
     dialect: "mysql",
   }
 );
