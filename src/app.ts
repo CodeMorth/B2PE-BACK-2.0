2971a84a1972b09a6c8bd553bf5d1620b3e16a70
Droid-Jr <droidjr10@gmail.com>
2024-08-29 22:46:14 -0500

    <utils>:agrege funciones para usar las utilidades de las librerias , <router>:agrege ruta y asigne en el app.ts ,<middleware>:agrege el multer middleware , <Interface>:agrege interface

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, {Request , Response} from "express";
+import { apiRutas } from "./routes";
+import morgan from "morgan";
 require("dotenv").config();
+import cors from 'cors';
 
 const app = express();
 
-app.get("/", (req: any, res: any) => {
+app.use(express.json());
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+
+//rutas
+apiRutas(app)
+
+app.get("/", (req:Request, res:Response) => {
   res.send("Bienvenido a mi servidor");
 });
 
